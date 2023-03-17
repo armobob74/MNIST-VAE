ce8bd627648411a1ce77ff6b570ec619dcded956
Armon <armon@nudgex.net>
2023-03-17 11:35:27 -0700

    laid out skeleton for VAE

---
+++ model.py
@@ -0,0 +1,96 @@
+from torch.utils.data import DataLoader
+from torchvision.datasets import MNIST
+from torchvision.transforms import ToTensor
+from tqdm import tqdm
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+class Encoder(nn.Module):
+    def __init__(self, latent_dim):
+        super(Encoder, self).__init__()
+        self.conv1 = nn.Conv2d(1, 32, kernel_size=3, stride=1, padding=1)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, stride=2, padding=1)
+        self.conv3 = nn.Conv2d(64, 128, kernel_size=3, stride=2, padding=1)
+        self.fc1 = nn.Linear(128*7*7, 512)
+        self.fc21 = nn.Linear(512, latent_dim)
+        self.fc22 = nn.Linear(512, latent_dim)
+
+    def forward(self, x):
+        x = nn.functional.relu(self.conv1(x))
+        x = nn.functional.relu(self.conv2(x))
+        x = nn.functional.relu(self.conv3(x))
+        x = x.view(-1, 128*7*7)
+        x = nn.functional.relu(self.fc1(x))
+        mean = self.fc21(x)
+        logvar = self.fc22(x)
+        return mean, logvar
+
+class Decoder(nn.Module):
+    def __init__(self, latent_dim):
+        super(Decoder, self).__init__()
+        self.fc1 = nn.Linear(latent_dim, 512)
+        self.fc2 = nn.Linear(512, 128*7*7)
+        self.conv1 = nn.ConvTranspose2d(128, 64, kernel_size=3, stride=2, padding=1, output_padding=1)
+        self.conv2 = nn.ConvTranspose2d(64, 32, kernel_size=3, stride=2, padding=1, output_padding=1)
+        self.conv3 = nn.ConvTranspose2d(32, 1, kernel_size=3, stride=1, padding=1)
+
+    def forward(self, z):
+        z = nn.functional.relu(self.fc1(z))
+        z = nn.functional.relu(self.fc2(z))
+        z = z.view(-1, 128, 7, 7)
+        z = nn.functional.relu(self.conv1(z))
+        z = nn.functional.relu(self.conv2(z))
+        x = torch.sigmoid(self.conv3(z))
+        return x
+
+def train_vae_on_mnist(latent_dim=20, batch_size=128, num_epochs=10, learning_rate=1e-3):
+    train_dataset = MNIST(root='./data', train=True, transform=ToTensor(), download=True)
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model = VAE(latent_dim=latent_dim).to(device)
+
+    optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+    criterion = nn.BCELoss(reduction='sum')
+
+    model.train()
+    for epoch in range(num_epochs):
+        train_loss = 0
+        for batch_idx, (data, _) in tqdm(enumerate(train_loader), total=len(train_loader), desc=f"Epoch {epoch+1}/{num_epochs}"):
+            data = data.to(device)
+            optimizer.zero_grad()
+            recon_batch, mu, logvar = model(data)
+            loss = vae_loss(recon_batch, data, mu, logvar, criterion)
+            loss.backward()
+            train_loss += loss.item()
+            optimizer.step()
+
+        print(f"Epoch {epoch+1}/{num_epochs}, Train Loss: {train_loss/len(train_loader.dataset):.4f}")
+
+    torch.save(model.state_dict(), "vae_mnist.pth")
+
+
+def vae_loss(recon_x, x, mu, logvar, criterion):
+    BCE = criterion(recon_x, x)
+    KLD = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
+    return BCE + KLD
+
+
+def generate_samples_from_vae(model_path="vae_mnist.pth", num_samples=10):
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model = VAE(latent_dim=20).to(device)
+    model.load_state_dict(torch.load(model_path, map_location=device))
+    model.eval()
+
+    with torch.no_grad():
+        z = torch.randn(num_samples, 20).to(device)
+        samples = model.decoder(z).cpu()
+
+    import matplotlib.pyplot as plt
+    fig, axs = plt.subplots(1, num_samples, figsize=(num_samples, 1))
+    for i in range(num_samples):
+        axs[i].imshow(samples[i].reshape(28, 28), cmap='gray')
+        axs[i].axis('off')
+    plt.show()
+
